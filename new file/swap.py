0266937b1b6c67b232be23a274b4c4a0319b9578
Shisan Adhikari <shisanadhikari15@gmail.com>
2022-12-22 20:45:09 +0545

    first commit

---
+++ new file/swap.py
@@ -0,0 +1,9 @@
+a = int(input("please enter a number:"))
+b = int(input("please enter another number:"))
+
+print("Value of a : ", a, " and b : ", b)
+a, b = b, a
+
+print("Value of a : ", a, " and b : ", b)
+
+
